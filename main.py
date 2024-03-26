22e4e306bc3d0a1e1fed1db1d0b7db850037bfc3
antonovmike <antonovma@outlook.com>
2024-03-26 20:33:05 +0400

    Removed extra code

--- main.py
+++ main.py
@@ -31,28 +31,3 @@ scooter.change_status(ScooterStatus.AVAILABLE)
 employee.service_scooter(scooter, scooter.is_available())
 print("Client tries to rent scooter while it is in services")
 client.rent_scooter(scooter, scooter.is_available())
-
-# print("> Using the RentalSystem with different rental types")
-regular_rental = RegularRental(scooter)
-discounted_rental = DiscountedRental(scooter)
-service_rental = ServiceRental(scooter)
-
-rental_system = RentalSystem(regular_rental)
-rental_system.rent()
-
-# print("> Client reserves scooter")
-scooter = Scooter(ScooterStatus.RESERVED)
-status_checker.check_status(scooter)
-
-# print("> Discounted rental")
-rental_system = RentalSystem(discounted_rental)
-rental_system.rent()
-
-# print("> Service rental")
-rental_system = RentalSystem(service_rental)
-rental_system.rent()
-
-# print("> Termination of rent")
-scooter.change_status(ScooterStatus.AVAILABLE)
-# print("> Check status")
-status_checker.check_status(scooter)
