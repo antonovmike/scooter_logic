3d649015fa5e90039170d7875681c6b4f26bb3af
antonovmike <antonovma@outlook.com>
2024-04-12 08:33:28 +0400

    Simple FastAPI tests

--- app/main.py
+++ app/main.py
@@ -11,22 +11,23 @@ async def root():
     return {"message": "Rent a scooter!"}
 
 
-@app.get("/rent", status_code=status.HTTP_204_NO_CONTENT)
+@app.get("/rent")
 async def rent():
     scooter_status = client.take_scooter(scooter, scooter.is_available(False))
     print(scooter_status)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return {"message": scooter_status}
+    # return JSONResponse(content={"message": "rented"})
 
 
-@app.get("/service", status_code=status.HTTP_204_NO_CONTENT)
+@app.get("/service")
 async def service():
     scooter_status = employee.take_scooter(scooter, scooter.is_available(True))
     print(scooter_status)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return {"message": scooter_status}
 
 
-@app.get("/free", status_code=status.HTTP_204_NO_CONTENT)
+@app.get("/free")
 async def free():
     scooter_status = scooter.change_status(ScooterStatus.AVAILABLE)
     print(scooter_status)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return {"message": scooter_status}
--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
-
+from scooter.scooter import ScooterStatus
 
 client = TestClient(app)
 
@@ -12,16 +12,19 @@ def test_root():
     assert response.json() == {"message": "Rent a scooter!"}
 
 
-def test_rent_and_status():
+def test_rent():
     response = client.get("/rent")
-    assert response.status_code == 204
-
-
-def test_service():
-    response = client.get("/service")
-    assert response.status_code == 204
+    assert response.status_code == 200
+    assert response.json() == {"message": ScooterStatus.RENTED}
 
 
 def test_free():
     response = client.get("/free")
-    assert response.status_code == 204
+    assert response.status_code == 200
+    assert response.json() == {"message": ScooterStatus.AVAILABLE}
+
+
+def test_service():
+    response = client.get("/service")
+    assert response.status_code == 200
+    assert response.json() == {"message": ScooterStatus.SERVICE}
