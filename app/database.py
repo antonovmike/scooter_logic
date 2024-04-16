b74dda01105e5b80171ad72e59a4baf2c0cc693e
antonovmike <antonovma@outlook.com>
2024-04-16 18:26:41 +0400

    New DB name

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123@localhost/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123@localhost/scooters'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
