65adc8979aae2b6d882cfa68fe0f09adcba11e9e
antonovmike <antonovma@outlook.com>
2024-04-24 15:33:17 +0400

    Refactor

--- app/routers/rout_rent.py
+++ app/routers/rout_rent.py
@@ -20,62 +20,22 @@ class InvalidScooterStatusError(Exception):
 
 @router.get("/rent/{scooter_id}")
 async def rent(scooter_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user.email)
-
-    scooter_logic = check_scooter_availability(db, scooter_id, current_user)
-
-    scooter = db.query(Scooter).filter(Scooter.id == scooter_id).first()
+    scooter, scooter_logic = get_scooter_and_check_availability(db, scooter_id, current_user)
 
     if scooter_logic.battery.get_level() <= battery_crytical:
         raise HTTPException(status_code=400, detail="Scooter battery level is too low to rent")
 
-    try:
-        scooter_logic.change_status(ScooterStatus.RENTED)
-    except InvalidScooterStatusError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    scooter.status = scooter_logic.status
-
-    scooter_logic.decrease_battery(17)
-    scooter.battery_level = scooter_logic.battery.get_level()
-
-    add_to_scooter_log(db, scooter_id, scooter.status, current_user.id)
-
-    db.commit()
+    update_scooter_status_and_battery(db, scooter, scooter_logic, ScooterStatus.RENTED, current_user)
 
     return {"message": f"Scooter {scooter_id} is now rented"}
 
 
 @router.get("/service/{scooter_id}")
 async def service(scooter_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    scooter_logic = check_scooter_availability(db, scooter_id, current_user)
+    scooter, scooter_logic = get_scooter_and_check_availability(db, scooter_id, current_user)
 
-    print(current_user.email)
-    employee = db.query(User).filter(User.id == current_user.id).first()
-
-    if employee.is_user_employee:
-        scooter = db.query(Scooter).filter(Scooter.id == scooter_id).first()
-
-        if not scooter:
-            raise HTTPException(status_code=404, detail="Scooter not found")
-
-        battery = Battery(level=scooter.battery_level)
-        scooter_logic = ScooterLogic(scooter.status, battery)
-
-        try:
-            scooter_logic.change_status(ScooterStatus.SERVICE)
-        except InvalidScooterStatusError as e:
-            raise HTTPException(status_code=400, detail=str(e))
-
-        scooter.status = scooter_logic.status
-
-        scooter_logic.charge_battery()
-        scooter.battery_level = scooter_logic.battery.get_level()
-
-        add_to_scooter_log(db, scooter_id, scooter.status, current_user.id)
-
-        db.commit()
-
+    if current_user.is_user_employee:
+        update_scooter_status_and_battery(db, scooter, scooter_logic, ScooterStatus.SERVICE, current_user)
         return {"message": f"Scooter {scooter_id} is now in service"}
     else:
         return {"message": f"You are not an employee"}
@@ -133,3 +93,31 @@ def check_scooter_availability(db: Session, scooter_id: int, current_user: int):
         raise HTTPException(status_code=400, detail=f"Scooter is not available: {scooter.status}")
 
     return scooter_logic
+
+
+def get_scooter_and_check_availability(db: Session, scooter_id: int, current_user: int):
+    scooter = db.query(Scooter).filter(Scooter.id == scooter_id).first()
+    if not scooter:
+        raise HTTPException(status_code=404, detail="Scooter not found")
+
+    battery = Battery(level=scooter.battery_level)
+    scooter_logic = ScooterLogic(scooter.status, battery)
+
+    if not scooter_logic.is_available(current_user.is_user_employee):
+        raise HTTPException(status_code=400, detail=f"Scooter is not available: {scooter.status}")
+
+    return scooter, scooter_logic
+
+
+def update_scooter_status_and_battery(db: Session, scooter: Scooter, scooter_logic: ScooterLogic, new_status: ScooterStatus, current_user, battery_change: int = 0):
+    try:
+        scooter_logic.change_status(new_status)
+    except InvalidScooterStatusError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    scooter.status = scooter_logic.status
+    scooter_logic.decrease_battery(battery_change)
+    scooter.battery_level = scooter_logic.battery.get_level()
+
+    add_to_scooter_log(db, scooter.id, scooter.status, current_user.id)
+    db.commit()
