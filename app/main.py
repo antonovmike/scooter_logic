ad9495e0942cadf769f6321c69d7c6351b93b932
antonovmike <antonovma@outlook.com>
2024-05-01 13:01:58 +0400

    commented 'while' in app/main.py

--- app/main.py
+++ app/main.py
@@ -14,21 +14,21 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', 
-            password='123', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        # If I set up wrong password start uvicorn app.main:app --reload
-        # see this error, then change password to correct one I will still
-        # in the loop. Even Ctrl+C doesn't help. But in the video this code
-        # works fine: https://youtu.be/0sOvCWFmrtA?feature=shared&t=14803
-        time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', 
+#             password='123', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successful")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         # If I set up wrong password start uvicorn app.main:app --reload
+#         # see this error, then change password to correct one I will still
+#         # in the loop. Even Ctrl+C doesn't help. But in the video this code
+#         # works fine: https://youtu.be/0sOvCWFmrtA?feature=shared&t=14803
+#         time.sleep(2)
 
 
 app.include_router(rout_rent.router)
