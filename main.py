bd8abde31cccc46495c3978feb61d9c9a6b7cb0d
antonovmike <antonovma@outlook.com>
2024-05-13 10:25:17 +0400

    fixed Example Usage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scooter.scooter import (
+    Battery,
     Scooter,
     ScooterStatus,
     ScooterStatusChecker,
@@ -7,27 +8,28 @@ from scooter.scooter import (
 from scooter.user import Client, Employee
 
 # Example Usage
-scooter = Scooter(ScooterStatus.AVAILABLE)
+battery = Battery(100)
+scooter = Scooter(ScooterStatus.AVAILABLE, battery=battery)
 client = Client()
 employee = Employee()
 status_checker = ScooterStatusChecker()
 
 print("> Client rents a scooter")
-client.take_scooter(scooter, scooter.is_available())
+client.take_scooter(scooter, scooter.is_available(False))
 print("> Client tries to rent rented scooter")
-client.take_scooter(scooter, scooter.is_available())
+client.take_scooter(scooter, scooter.is_available(False))
 
 print("> Employee tries to service rented scooter")
-employee.take_scooter(scooter, scooter.is_available())
+employee.take_scooter(scooter, scooter.is_available(True))
 
 print("> Termination of rent")
 scooter.change_status(ScooterStatus.AVAILABLE)
 print("> Client rents a scooter")
-client.take_scooter(scooter, scooter.is_available())
+client.take_scooter(scooter, scooter.is_available(False))
 print("> Termination of rent")
 scooter.change_status(ScooterStatus.AVAILABLE)
 
 print("> Employee services a scooter")
-employee.take_scooter(scooter, scooter.is_available())
+employee.take_scooter(scooter, scooter.is_available(True))
 print("> Client tries to rent scooter while it is in services")
-client.take_scooter(scooter, scooter.is_available())
+client.take_scooter(scooter, scooter.is_available(False))
