41f9ad7c2bb15a9eacbd23dffa59b4f7f51dcf20
antonovmike <antonovma@outlook.com>
2024-05-14 14:23:33 +0400

    main.py ranamed to example_usage.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 import sqlalchemy
 
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from .config import settings
